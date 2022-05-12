f88764f287124ffe1f6d98f0c3a17c6ca8d056c7
nguyendieua1 <31350108+nguyendieua1@users.noreply.github.com>
2022-05-12 13:09:13 +0700

    update

--- JsBasic.js
+++ JsBasic.js
@@ -5,8 +5,26 @@ var products = [
     {name: 'ip 9', price: 4000000}, 
 ]
 
-var tongtien = products.reduce(function(tong,product){
-    return tong + product.price
-},0)
+// var tongtien = products.reduce(function(tong,product){
+//     return tong + product.price
+// },0)
+
+
+//reduce
+var tongtien = products.reduce(function(tongtam,product){
+    if(product.price >= 3000000){
+        return tongtam + product.price
+     }     
+     return tongtam
+
+} ,0)
 
 console.log(tongtien)
+
+//forEach 
+
+products.forEach(product=>{
+    console.log(product)
+})
+
+//some()
--- JsBasic2.js
+++ JsBasic2.js
@@ -1,4 +1,4 @@
-var n = 30;
+var n = 19;
 
 for(i = 0;i < n; i ++ ){
     console.log(i)
@@ -7,4 +7,15 @@ for(i = 0;i < n; i ++ ){
 //break out vong lap
 
 
-for()
+for(j = 0;j < n; j ++ ){
+    if(j == 10){
+        break;
+    }
+    console.log(j)
+}
+// while
+var k = 0;
+while (k <= n) {
+    console.log(k);
+    k ++ ;
+}
